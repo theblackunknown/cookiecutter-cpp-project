34e8faa7ddc14844ea910da383df159c0fe44849
MACHIZAUD Andréa <andrea.machizaud@gmail.com>
2019-09-22 23:45:03 +0200

    more refinments

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,7 +2,7 @@
 
 if __name__ == '__main__':
     """Initialize a git repository based on the configured branch and repo"""
-    url    = 'git@github.com:{{ cookiecutter.github_username }}/{{ cookiecutter.project_name }}'
+    url    = 'git@{{ cookiecutter.github_url }}:{{ cookiecutter.github_username }}/{{ cookiecutter.project_name }}.git'
     branch = '{{ cookiecutter.branch }}'
     remote = '{{ cookiecutter.remote }}'
 
